203df0f7e33bd630460fb81535ea559c42a75f5b
Daniel <daniel.martos@hotmail.com>
2025-10-26 13:49:08 +0100

    fix insights

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./i18n";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
